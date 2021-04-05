5c64ed768bf08a3090dc222e700c491fe4dde1ea
kabeerSohail7 <kabeersohel@gmail.com>
2021-04-04 23:41:15 -0800

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontendApp';
+export class AppComponent implements OnInit {
+
+  ngOnInit(){
+    this.getEmployees();
+  }
+
+  public employees : Employee[];
+
+  constructor(private employeeService: EmployeeService){}
+
+  public getEmployees() : void{
+    this.employeeService.getEmployees().subscribe(
+      (response:Employee[]) => {
+        this.employees = response;
+      },
+      (error:HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    );
+  }
+
 }
